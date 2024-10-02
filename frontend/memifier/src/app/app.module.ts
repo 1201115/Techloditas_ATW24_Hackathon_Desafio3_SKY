7c18c1669ae6dd7ec5acc076029ff5c96a61d426
1201115 <1201115@isep.ipp.pt>
2024-10-02 19:24:12 +0100

    Update app.module.ts

--- frontend/memifier/src/app/app.module.ts
+++ frontend/memifier/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
