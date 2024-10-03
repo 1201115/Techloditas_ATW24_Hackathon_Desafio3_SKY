5ec7ae779dfe6e5d70afa8b20b385851940c2f9e
Miguel Almeida <92300965+1201115@users.noreply.github.com>
2024-10-03 20:11:06 +0100

    Added steps

--- frontend/memifier/src/app/app.module.ts
+++ frontend/memifier/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { HomeComponent } from './components/home/home.component';
 import { VideoTrimmerComponent } from './components/video-trimmer/video-trimmer.component';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
+import { EditComponent } from './components/edit/edit.component';
+import { ExportComponent } from './components/export/export.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, VideoTrimmerComponent],
+  declarations: [AppComponent, HomeComponent, VideoTrimmerComponent, EditComponent, ExportComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
--- frontend/memifier/src/app/components/home/home.component.ts
+++ frontend/memifier/src/app/components/home/home.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  currentStep = 1;
 
+  nextStep() {
+    if (this.currentStep < 3) {
+      this.currentStep++;
+    }
+  }
+
+  previousStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
 }
